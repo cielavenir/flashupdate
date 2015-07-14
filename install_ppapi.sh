e26291802cf19b89928f30ab323195ee084e2267
cielavenir <cielartisan@gmail.com>
2015-07-15 00:42:11 +0900

    add PPAPI version

--- install_ppapi.sh
+++ install_ppapi.sh
@@ -9,14 +9,14 @@ if [ `whoami` == "root" ];then
 	export HOME=""
 fi
 
-FLASH_URL="http://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player_osx.dmg"
+FLASH_URL="http://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player_osx_ppapi.dmg"
 FLASH_DMG=$(mktemp -d -t flash_dmg)/flash.dmg
 FLASH_MOUNTPOINT=$(mktemp -d -t flash_mountpoint)
  
 curl -o $FLASH_DMG $FLASH_URL
 hdiutil attach -mountpoint $FLASH_MOUNTPOINT -nobrowse $FLASH_DMG
  
-FLASH_PKG_ARCHIVE="$FLASH_MOUNTPOINT/Install Adobe Flash Player.app/Contents/Resources/Adobe Flash Player.pkg"
+FLASH_PKG_ARCHIVE="$FLASH_MOUNTPOINT/Install Adobe Pepper Flash Player.app/Contents/Resources/Adobe Flash Player.pkg"
 FLASH_PKG_EXTRACT_DIR=$(mktemp -d -t flash_pkg_extract_dir)
 FLASH_INSTALL_SRC="$FLASH_PKG_EXTRACT_DIR/AdobeFlashPlayerComponent.pkg"
  
@@ -27,11 +27,11 @@ cd "$FLASH_INSTALL_SRC"
 cat Payload | gunzip -dc | cpio -i
 cat Scripts | gunzip -dc | cpio -i
  
-mkdir -p "$HOME/Library/Internet Plug-Ins"
+mkdir -p "$HOME/Library/Internet Plug-Ins/PepperFlashPlayer"
 mkdir -p "$HOME/Library/PreferencePanes"
  
-cp -f "$FLASH_INSTALL_SRC/Library/Internet Plug-Ins/Flash Player.plugin.lzma" "$HOME/Library/Internet Plug-Ins"
-cp -f "$FLASH_INSTALL_SRC/Library/Internet Plug-Ins/flashplayer.xpt" "$HOME/Library/Internet Plug-Ins"
+cp -f "$FLASH_INSTALL_SRC/Library/Internet Plug-Ins/PepperFlashPlayer/PepperFlashPlayer.plugin.lzma" "$HOME/Library/Internet Plug-Ins/PepperFlashPlayer"
+cp -f "$FLASH_INSTALL_SRC/Library/Internet Plug-Ins/PepperFlashPlayer/manifest.json" "$HOME/Library/Internet Plug-Ins/PepperFlashPlayer"
 cp -fr "$FLASH_INSTALL_SRC/Library/PreferencePanes/Flash Player.prefPane" "$HOME/Library/PreferencePanes"
  
 # extracts the lzma archive in ~/Library/Internet Plug-Ins
